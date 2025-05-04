e3f492e7e2c5de4180d433d1b4879de924822a21
Amir <baradaran13085@gmail.com>
2025-05-04 12:39:37 +0330

    End English Language

--- src/Pages/Portfolio/Sites/index.jsx
+++ src/Pages/Portfolio/Sites/index.jsx
@@ -1,125 +1,157 @@
 import React, { useState } from "react";
-import { Card, CardMedia, Button, Typography, Box, Modal, Stack } from "@mui/material";
+import {
+  Card,
+  CardMedia,
+  Button,
+  Typography,
+  Box,
+  Modal,
+  Grid,
+  Stack,
+} from "@mui/material";
 import res from "../../../assets/Resturant.jpg";
 import motor from "../../../assets/motorx.jpg";
+import digitalShop from "../../../assets/figma(1).jpg";
+import electroShop from "../../../assets/electro.png";
+import loginForm from "../../../assets/login-form-sofa.jpg";
 
 const data = [
   {
     image: res,
     title: "Restaurant and Cafe Design Project",
-    description: "This project is created to showcase my visual design skills in the restaurant and cafe industry.",
+    description:
+      "This project is created to showcase my visual design skills in the restaurant and cafe industry.",
   },
   {
     image: motor,
     title: "Motor Layout",
-    description: "A modern Motor design focused on user experience and interior aesthetics.",
+    description:
+      "A modern Motor design focused on user experience and interior aesthetics.",
+  },
+  {
+    image: digitalShop,
+    title: "digitalShop Layout",
+    description:
+      "A modern digitalShop design focused on user experience and interior aesthetics.",
+  },
+  {
+    image: electroShop,
+    title: "ElectroShop Layout",
+    description:
+      "A modern ElectroShop design focused on user experience and interior aesthetics.",
+  },
+  {
+    image: loginForm,
+    title: "LoginForm Layout",
+    description:
+      "A modern LoginForm design focused on user experience and interior aesthetics.",
   },
 ];
 
 export default function Sites() {
   const [open, setOpen] = useState(false);
-  const [hoveredIndex, setHoveredIndex] = useState(null); // وضعیت جدید برای مشخص کردن کارت هاور شده
+  const [hoveredIndex, setHoveredIndex] = useState(null);
   const [selectedProject, setSelectedProject] = useState(null);
 
   return (
-    <Stack direction={"row"} gap={10}>
-      {/* کارت‌ها */}
-      {data.map((project, index) => (
-        <Card
-          key={index}
-          sx={{
-            maxWidth: "100%",
-            height: "50vh",
-            boxShadow: 6,
-            borderRadius: 4,
-            overflow: "hidden",
-            position: "relative",
-            transition: "all 0.3s ease-in-out",
-            cursor: "pointer",
-            "&:hover": {
-              transform: "scale(1.05)",
-              boxShadow: "0px 8px 20px rgba(0,0,0,0.3)",
-            },
-          }}
-          onMouseEnter={() => setHoveredIndex(index)} // تنها برای کارت مورد نظر
-          onMouseLeave={() => setHoveredIndex(null)} // غیرفعال کردن هاور
-        >
-          <CardMedia
-            component="img"
-            alt={project.title}
-            image={project.image}
-            sx={{
-              objectFit: "cover",
-              width: "100%",
-              height: "100%",
-              transition: "transform 0.5s ease-in-out",
-              transform: hoveredIndex === index ? "scale(1.1)" : "scale(1)", // تنها برای کارت هاور شده
-            }}
-          />
-
-          {/* دکمه روی عکس هنگام هاور */}
-          {hoveredIndex === index && (
-            <Box
+    <>
+      <Grid container spacing={4}>
+        {data.map((project, index) => (
+          <Grid item xs={12} sm={6} md={4} key={index}>
+            <Card
               sx={{
-                position: "absolute",
-                top: 0,
-                left: 0,
-                width: "100%",
-                height: "100%",
-                background:
-                  "linear-gradient(to top, rgba(0,0,0,0.7), rgba(0,0,0,0.2))",
-                display: "flex",
-                justifyContent: "center",
-                alignItems: "center",
-                transition: "all 0.4s ease-in-out",
-                opacity: 1,
+                height: "50vh",
+                boxShadow: 6,
+                borderRadius: 4,
+                overflow: "hidden",
+                position: "relative",
+                transition: "all 0.3s ease-in-out",
+                cursor: "pointer",
+                "&:hover": {
+                  transform: "scale(1.05)",
+                  boxShadow: "0px 8px 20px rgba(0,0,0,0.3)",
+                },
               }}
+              onMouseEnter={() => setHoveredIndex(index)}
+              onMouseLeave={() => setHoveredIndex(null)}
             >
-              <Button
-                variant="outlined"
+              <CardMedia
+                component="img"
+                alt={project.title}
+                image={project.image}
                 sx={{
-                  color: "#fff",
-                  borderColor: "#fff",
-                  borderRadius: 10,
-                  py: 1.5,
-                  fontWeight: "bold",
-                  fontSize: "0.8rem",
-                  textShadow: "0 1px 3px rgba(0,0,0,0.6)",
-                  backdropFilter: "blur(4px)",
-                  transition: "all 0.3s ease",
-                  "&:hover": {
-                    backgroundColor: "rgba(255, 255, 255, 0.2)",
-                    transform: "scale(1.1)",
-                  },
+                  objectFit: "cover",
+                  width: "300px",
+                  height: "100%",
+                  transition: "transform 0.5s ease-in-out",
+                  transform: hoveredIndex === index ? "scale(1.1)" : "scale(1)",
                 }}
-                onClick={() => {
-                  setSelectedProject(project);
-                  setOpen(true);
-                }}
-              >
-                See More
-              </Button>
-            </Box>
-          )}
-        </Card>
-      ))}
+              />
+              {hoveredIndex === index && (
+                <Box
+                  sx={{
+                    position: "absolute",
+                    top: 0,
+                    left: 0,
+                    width: "100%",
+                    height: "100%",
+                    background:
+                      "linear-gradient(to top, rgba(0,0,0,0.7), rgba(0,0,0,0.2))",
+                    display: "flex",
+                    justifyContent: "center",
+                    alignItems: "center",
+                    transition: "all 0.4s ease-in-out",
+                    opacity: 1,
+                  }}
+                >
+                  <Button
+                    variant="outlined"
+                    sx={{
+                      color: "#fff",
+                      borderColor: "#fff",
+                      borderRadius: 10,
+                      py: 1.5,
+                      px: 1.5,
+                      fontWeight: "bold",
+                      fontSize: "0.7rem",
+                      textShadow: "0 1px 3px rgba(0,0,0,0.6)",
+                      backdropFilter: "blur(4px)",
+                      transition: "all 0.3s ease",
+                      "&:hover": {
+                        backgroundColor: "rgba(255, 255, 255, 0.2)",
+                        transform: "scale(1.1)",
+                      },
+                    }}
+                    onClick={() => {
+                      setSelectedProject(project);
+                      setOpen(true);
+                    }}
+                  >
+                    See More
+                  </Button>
+                </Box>
+              )}
+            </Card>
+          </Grid>
+        ))}
+      </Grid>
 
-      {/* حالت تمام‌صفحه */}
       <Modal
         open={open}
         onClose={() => setOpen(false)}
         sx={{ display: "flex", alignItems: "center", justifyContent: "center" }}
       >
-        <Box
+        <Stack
+          gap={5}
           sx={{
             width: "100%",
-            height: "100%",
+            maxHeight: "100vh",
             bgcolor: "rgba(0,0,0,0.95)",
-            display: "flex",
             flexDirection: { xs: "column", md: "row" },
             alignItems: "center",
             justifyContent: "center",
-            p: 4,
+            overflowY: "auto",
+            p: { xs: 2, md: 4 },
             borderRadius: 2,
             boxShadow: 24,
           }}
@@ -137,23 +169,28 @@ export default function Sites() {
               mb: { xs: 2, md: 0 },
               transition: "transform 0.5s ease-in-out",
               transform: "scale(1.05)",
+              mt: { xs: 30, sm: 0 },
             }}
           />
-
-          <Box
+          <Stack
+            justifyContent={"center"}
             sx={{
               color: "#fff",
               textAlign: "center",
               ml: { md: 4 },
-              mt: { xs: 2, md: 0 },
+              mt: { xs: -2, md: 0 },
               maxWidth: 500,
               animation: "fadeIn 0.5s ease-in-out",
+              height: "100vh",
             }}
           >
             <Typography
               variant="h4"
               gutterBottom
-              sx={{ textShadow: "0 2px 6px rgba(0,0,0,0.5)", fontSize: "2rem" }}
+              sx={{
+                textShadow: "0 2px 6px rgba(0,0,0,0.5)",
+                fontSize: "2rem",
+              }}
             >
               {selectedProject?.title}
             </Typography>
@@ -180,14 +217,15 @@ export default function Sites() {
                 py: 1.5,
                 px: 4,
                 fontSize: "1rem",
+                mb: { xs: 5 },
               }}
               onClick={() => setOpen(false)}
             >
               Close
             </Button>
-          </Box>
-        </Box>
+          </Stack>
+        </Stack>
       </Modal>
-    </Stack>
+    </>
   );
 }
